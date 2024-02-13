bc5cb06222e5c32f93f8e9c7749abca29f0e37e0
David Granger <dpgranger8@gmail.com>
2024-02-13 11:43:36 -0700

    Initial Commit

---
+++ GPT4Client/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GPT4Client
+//
+//  Created by David Granger on 2/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GPT4Client/GPT4ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPT4ClientApp.swift
+//  GPT4Client
+//
+//  Created by David Granger on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct GPT4ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
